31b0f319ceb2f721e9895ef069c319c0df9c80d6
Твоё Имя <ksenia.com>
2026-04-11 19:04:09 +0300

    Fix URLs configuration

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,3 +1,17 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import PostViewSet, GroupViewSet, CommentViewSet, FollowViewSet
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet)
+router.register(r'groups', GroupViewSet)
+router.register(r'follow', FollowViewSet, basename='follow')
 
 urlpatterns = [
+    path('', include(router.urls)),
+    path('posts/<int:post_id>/comments/', 
+         CommentViewSet.as_view({'get': 'list', 'post': 'create'})),
+    path('posts/<int:post_id>/comments/<int:pk>/',
+         CommentViewSet.as_view({'get': 'retrieve', 'put': 'update', 
+                                 'patch': 'partial_update', 'delete': 'destroy'})),
 ]
