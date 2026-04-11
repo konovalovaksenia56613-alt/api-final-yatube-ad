2339bc6b3b45ce5d6736ed5e03d3699c4eeb26b7
Твоё Имя <ksenia.com>
2026-04-11 19:15:29 +0300

    Add serializers for all models

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,23 +1,39 @@
 from rest_framework import serializers
-from rest_framework.relations import SlugRelatedField
-
-
-from posts.models import Comment, Post
+from posts.models import Post, Group, Comment, Follow
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = serializers.StringRelatedField(read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = ('id', 'title', 'slug', 'description')
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
-    )
+    author = serializers.StringRelatedField(read_only=True)
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = ('id', 'author', 'post', 'text', 'created')
+        read_only_fields = ('post',)
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.StringRelatedField(read_only=True)
+    following = serializers.StringRelatedField()
+
+    class Meta:
+        model = Follow
+        fields = ('user', 'following')
+
+    def validate_following(self, value):
+        if self.context['request'].user.username == value:
+            raise serializers.ValidationError('Нельзя подписаться на себя')
+        return value
