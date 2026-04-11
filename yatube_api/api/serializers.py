5851b23b549f40b8cfeab7dfe05a9cd1c8085682
Твоё Имя <ksenia.com>
2026-04-11 19:26:31 +0300

    Fix import order in serializers.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import get_user_model
-User = get_user_model()
-
 from rest_framework import serializers
 from posts.models import Post, Group, Comment, Follow
+
+User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
