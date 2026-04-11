5531e9887a8fa88822f61b863ade922997dfc6f7
Твоё Имя <ksenia.com>
2026-04-11 19:00:02 +0300

    Fix line length in models.py

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -14,7 +14,8 @@ class Post(models.Model):
         upload_to='posts/', null=True, blank=True
     )
     group = models.ForeignKey(
-        'Group', on_delete=models.SET_NULL, null=True, blank=True, related_name='posts'
+        'Group', on_delete=models.SET_NULL,
+        null=True, blank=True, related_name='posts'
     )
 
     def __str__(self):
