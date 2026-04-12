b7ca4ed9a3130c1e8f8d479dc89502d0d7292299
Твоё Имя <ksenia.com>
2026-04-12 11:02:36 +0300

    Remove unused Response import

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets, permissions, filters
-from rest_framework.response import Response
 from rest_framework.generics import get_object_or_404
 from rest_framework.exceptions import PermissionDenied
 from posts.models import Post, Group, Comment, Follow
