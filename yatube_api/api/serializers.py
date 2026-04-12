efe9739ef0787b087dda772421dd05c471815122
Твоё Имя <ksenia.com>
2026-04-12 12:17:24 +0300

    Simplify serializers: use StringRelatedField and all fields

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -6,49 +6,35 @@ User = get_user_model()
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.SerializerMethodField()
+    author = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Post
-        fields = ('id', 'text', 'pub_date', 'author', 'image', 'group')
-
-    def get_author(self, obj):
-        print(f"Post author: {obj.author.username}")
-        return obj.author.username
+        fields = '__all__'
 
 
 class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ('id', 'title', 'slug', 'description')
+        fields = '__all__'
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SerializerMethodField()
+    author = serializers.StringRelatedField(read_only=True)
 
     class Meta:
         model = Comment
-        fields = ('id', 'author', 'post', 'text', 'created')
+        fields = '__all__'
         read_only_fields = ('post',)
-
-    def get_author(self, obj):
-        print(f"Comment author: {obj.author.username}")
-        return obj.author.username
 
 
 class FollowSerializer(serializers.ModelSerializer):
-    user = serializers.SerializerMethodField()
-    following = serializers.SlugRelatedField(
-        slug_field='username',
-        queryset=User.objects.all()
-    )
+    user = serializers.StringRelatedField(read_only=True)
+    following = serializers.StringRelatedField()
 
     class Meta:
         model = Follow
-        fields = ('user', 'following')
-
-    def get_user(self, obj):
-        return obj.user.username
+        fields = '__all__'
 
     def validate_following(self, value):
         if self.context['request'].user == value:
